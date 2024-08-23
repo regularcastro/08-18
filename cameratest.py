5eabcf378da1f72eef4f91ccc1e19d13dfec8a8a
Guilherme Castro <castro.ethng@gmail.com>
2024-08-22 23:50:27 -0300

    damnson

--- cameratest.py
+++ cameratest.py
@@ -2,13 +2,15 @@ import cv2
 from rembg import remove
 
 def detect_lines(frame):
-    frame = remove(frame)
+    #frame = remove(frame)
     #gray = cv2.Canny(frame, 1,255,None,3)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    lsd = cv2.createLineSegmentDetector()
+    lsd = cv2.createLineSegmentDetector(refine= 3,scale= None,sigma_scale= None,quant= None,ang_th= None,log_eps= None,density_th= None, n_bins= None)
 
-    lines = lsd.detect(gray)[0]
+    #print(lsd.detect(gray)[0])                     ## coordenadas das linhas detectadas
+    lines = lsd.detect(gray, width=0,prec=None,nfa=None)[0]
     drawn_img = lsd.drawSegments(gray, lines)
+    print("nlines: ",len(lines))
     return drawn_img
 
 cap = cv2.VideoCapture(0)
